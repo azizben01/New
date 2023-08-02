ffdf8b71aa0371af83cdc9262c96b0632514ff84
azizben01 <141038328+azizben01@users.noreply.github.com>
2023-08-02 19:12:40 +0000

    first commit

--- goproject.go
+++ goproject.go
@@ -1,1 +1,22 @@
-#New
+//*package main
+
+//import "fmt"
+
+//func main() {
+//	fmt.Printf("HOLLA")
+//}
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	number := rand.Intn(10)
+	fmt.Println(number)
+	fmt.Printf("The time is", time.Now())
+
+}
