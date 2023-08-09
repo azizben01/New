66657671b09cb6f4a5cffc639e2db6d287af1275
Ben Aziz Sangaré <benazizsangare@bens-mbp.lan>
2023-08-09 21:26:15 +0000

    Second commit

--- goproject.go
+++ goproject.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import "fmt"
 
@@ -8,4 +8,32 @@ func swap(one, two string) (string, string) {
 func main() {
 	first, second := "Analized.", "Well"
 	fmt.Println(swap(first, second))
+}
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func reverseString(input string) string {
+	runes := []rune(input) // Convert the string to a slice of runes
+	length := len(runes)
+
+	// Swap characters from the beginning and end of the string
+	for i := 0; i < length/2; i++ {
+		runes[i], runes[length-i-1] = runes[length-i-1], runes[i]
+	}
+
+	return string(runes) // Convert the slice of runes back to a string
+}
+
+func main() {
+	fmt.Print("Enter a string: ")
+	var input string
+	fmt.Scanln(&input)
+
+	reversed := reverseString(input)
+	fmt.Println("Reversed string:", reversed)
 }
